98913c73bc71d7e031b5fb020939792d09f0acdb
Lindokuhle Sibeko <nicsibeko03@gmail.com>
2023-10-09 09:40:25 +0200

    add code 1

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var test int
+
+func main() {
+	//var card string = "Ace of Spades"
+	badCode := "Bad Code"
+	card := "Ace of Spades"
+	fmt.Println(card)
+	fmt.Println(badCode)
+
+	var test int
+	test = 91
+	fmt.Println(test)
+}
