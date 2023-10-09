f9905b0b5fef00e31ebdd6dd6ef23b357c597900
Lindokuhle Sibeko <nicsibeko03@gmail.com>
2023-10-09 10:19:55 +0200

    for loops

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import "fmt"
 var test int
 
 func main() {
-	//var card string = "Ace of Spades"
-	badCode := "Bad Code"
-	card := "Ace of Spades"
-	fmt.Println(card)
-	fmt.Println(badCode)
+	cards := []string{newCard(), "Ace of Diamonds"}
+	cards = append(cards, "Six of Spades")
 
-	var test int
-	test = 91
-	fmt.Println(test)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+	//fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
